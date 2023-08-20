21476bf103fafc8af85658acc87a95d73d2abd51
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-08-20 15:22:06 -0300

    FIX: username field is unique

--- practice02/src/project/database/migrations/20230820172522_create-user.ts
+++ practice02/src/project/database/migrations/20230820172522_create-user.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
     table.uuid('token').index().notNullable()
-    table.text('username').notNullable()
+    table.text('username').unique().notNullable()
     table.text('password').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
