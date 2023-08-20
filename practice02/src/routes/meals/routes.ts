2cf0842a9e01a8c282e4a0116176d954186d8c62
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-08-20 16:55:12 -0300

    CHORE: adding get meal data

--- practice02/src/routes/meals/routes.ts
+++ practice02/src/routes/meals/routes.ts
@@ -3,6 +3,7 @@ import { IsAuthenticatedMiddleware } from '../../middlewares/auth'
 import { createMealSchema } from './schemas'
 import { database } from '../../project/database'
 import { randomUUID } from 'node:crypto'
+import { uuidParamSchema } from '../../utils/schemas'
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.addHook('preHandler', IsAuthenticatedMiddleware)
@@ -27,5 +28,17 @@ export async function mealsRoutes(app: FastifyInstance) {
     return await database('meals').where({
       user_id: req.user?.id,
     })
+  })
+
+  app.get('/:id', async (req, res) => {
+    const schema = uuidParamSchema.safeParse(req.params)
+    if (schema.success) {
+      const meal = await database('meals').where({ id: schema.data.id }).first()
+      return meal
+        ? res.status(200).send(meal)
+        : res.status(404).send({ error: 'Not Found' })
+    } else {
+      return res.status(400).send(schema.error.format())
+    }
   })
 }
