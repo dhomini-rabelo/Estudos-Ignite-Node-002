7707e49ff780c8c4a76913562ed25d3580171fe7
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-08-21 07:51:50 -0300

    test create-user route

---
+++ practice02/tests/routes/users.test.ts
@@ -0,0 +1,83 @@
+import { execSync } from 'node:child_process'
+import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
+import { app } from '../../src/project/server'
+import { randomUUID } from 'node:crypto'
+import request from 'supertest'
+
+beforeAll(async () => {
+  execSync('npm run knex migrate:latest')
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+describe('Test create-user route', async () => {
+  it('must create a user', async () => {
+    const userData = {
+      username: `test-${randomUUID()}`,
+      password: 'test12345678',
+    }
+
+    await request(app.server)
+      .post('/api/auth/create-user')
+      .send(userData)
+      .expect(204)
+  })
+
+  test('invalid body', async () => {
+    const response = await request(app.server)
+      .post('/api/auth/create-user')
+      .expect(400)
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        _errors: ['Required'],
+      }),
+    )
+  })
+
+  test('required fields error', async () => {
+    const userData = {}
+
+    const response = await request(app.server)
+      .post('/api/auth/create-user')
+      .send(userData)
+      .expect(400)
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        username: expect.objectContaining({
+          _errors: ['Required'],
+        }),
+        password: expect.objectContaining({
+          _errors: ['Required'],
+        }),
+      }),
+    )
+  })
+
+  test('short fields error', async () => {
+    const userData = {
+      username: `tes`,
+      password: 'tes',
+    }
+
+    const response = await request(app.server)
+      .post('/api/auth/create-user')
+      .send(userData)
+      .expect(400)
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        username: expect.objectContaining({
+          _errors: ['String must contain at least 4 character(s)'],
+        }),
+        password: expect.objectContaining({
+          _errors: ['String must contain at least 8 character(s)'],
+        }),
+      }),
+    )
+  })
+})
