88d194e4eb9d0d3295952806d46ff9ebd96520d8
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-08-22 07:18:11 -0300

    TEST: login route

--- practice02/tests/routes/users.test.ts
+++ practice02/tests/routes/users.test.ts
@@ -71,3 +71,84 @@ describe('Test create-user route', async () => {
     )
   })
 })
+
+describe('Test login route', async () => {
+  test('login success case', async () => {
+    const userData = {
+      username: `test-${randomUUID()}`,
+      password: 'test12345678',
+    }
+
+    await request(app.server).post('/api/auth/create-user').send(userData)
+
+    const response = await request(app.server)
+      .post('/api/auth/login')
+      .set('Content-Type', 'application/json')
+      .send(userData)
+      .expect(200)
+
+    expect(response.body.token).toEqual(expect.any(String))
+  })
+
+  test('login with invalid password', async () => {
+    const userData = {
+      username: `test-${randomUUID()}`,
+      password: 'test12345678',
+    }
+
+    await request(app.server).post('/api/auth/create-user').send(userData)
+
+    await request(app.server)
+      .post('/api/auth/login')
+      .set('Content-Type', 'application/json')
+      .send({ ...userData, password: 'invalid password' })
+      .expect(401)
+  })
+
+  test('login with unregistered username', async () => {
+    const userData = {
+      username: `test-${randomUUID()}`,
+      password: 'test12345678',
+    }
+
+    await request(app.server).post('/api/auth/create-user').send(userData)
+
+    await request(app.server)
+      .post('/api/auth/login')
+      .set('Content-Type', 'application/json')
+      .send({ ...userData, password: 'invalid password' })
+      .expect(401)
+  })
+
+  test('invalid body', async () => {
+    const response = await request(app.server)
+      .post('/api/auth/login')
+      .expect(400)
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        _errors: ['Required'],
+      }),
+    )
+  })
+
+  test('required fields error', async () => {
+    const userData = {}
+
+    const response = await request(app.server)
+      .post('/api/auth/login')
+      .send(userData)
+      .expect(400)
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        username: expect.objectContaining({
+          _errors: ['Required'],
+        }),
+        password: expect.objectContaining({
+          _errors: ['Required'],
+        }),
+      }),
+    )
+  })
+})
